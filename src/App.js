0283cd36bdaa7d4ecdb8fa9f9c0f135def9b2ab1
HafizN <hafizn2013@outlook.com>
2020-12-02 02:05:46 -0800

    form sample for blog post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const URL = 'YOUR API ENDPOINT';
+
+  const handleInput = (input) => {
+
+		switch (input.target.name) {
+			case "email":
+				setEmail(input.target.value);
+				break;
+			case "subject":
+				setSubject(input.target.value);
+				break;
+			case "message":
+				setMessage(input.target.value);
+				break;
+		}
+  }
+  
+  const handleRequest = (e) => {
+    e.preventDefault();
+
+    const data = {
+      subject,
+      email,
+      desc: message
+    };
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(data)
+    };
+
+    fetch(URL, options)
+      .then(() => {
+        console.log('form was sent');
+      })
+      .catch(err => console.log(err));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Contact Form</h2>
+      <form>
+        <label htmlFor="subject">Subject</label>
+        <input type="text" name="subject" onChange={handleInput}/>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" onChange={handleInput}/>
+        <label htmlFor="message">Message</label>
+        <textarea name="message" onChange={handleInput}/>
+        <input type="submit" onClick={handleRequest} />
+      </form>
     </div>
   );
 }
